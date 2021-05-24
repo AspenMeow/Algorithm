be14783b700be93ab108b7dfbde28c16c136109d
Di Chen <chendi1020@gmail.com>
2021-05-23 22:57:40 -0400

    add hw3

---
+++ BruteCollinearPoints.java
@@ -0,0 +1,84 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BruteCollinearPoints {
+    private int i;
+    private LineSegment[] lsarry;
+
+    public BruteCollinearPoints(Point[] points) {
+        i = 0;
+        int len = points.length;
+        lsarry = new LineSegment[len];
+        for (int p = 0; p < len - 3; p++) {
+            for (int q = p + 1; q < len - 2; q++) {
+                for (int r = q + 1; r < len - 1; r++) {
+                    for (int s = r + 1; s < len; s++) {
+                        ArrayList<Point> pl = new ArrayList<Point>();
+                        Double s1 = points[p].slopeTo(points[q]);
+                        Double s2 = points[p].slopeTo(points[r]);
+                        Double s3 = points[p].slopeTo(points[s]);
+                        if (s1.equals(s2) && s1.equals(s3)) {
+                            pl.add(points[p]);
+                            pl.add(points[r]);
+                            pl.add(points[q]);
+                            pl.add(points[s]);
+                            Collections.sort(pl);
+                            LineSegment ls = new LineSegment(pl.get(0), pl.get(3));
+                            lsarry[i] = ls;
+                            i++;
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public int numberOfSegments() {
+        return i;
+    }
+
+    public LineSegment[] segments() {
+        LineSegment[] newls = new LineSegment[i];
+        for (int j = 0; j < i; j++) newls[j] = lsarry[j];
+        return newls;
+    }
+
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+
+
+    }
+
+
+}
---
+++ FastCollinearPoints.java
@@ -0,0 +1,111 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class FastCollinearPoints {
+    private LineSegment[] lsarr;
+    private int couter = 0;
+    private ArrayList<Point> found;
+
+
+    public FastCollinearPoints(Point[] points) {
+        if (points == null) throw new IllegalArgumentException("");
+        int len = points.length;
+        found = new ArrayList<Point>();
+        lsarr = new LineSegment[points.length];
+        Arrays.sort(points);
+        for (int i = 0; i < len - 3; i++) {
+            Point p = points[i];
+            if (exist(found, p)) continue;
+            ArrayList<Point> ap = new ArrayList<Point>();
+            for (int j = i + 1; j < len; j++) ap.add(points[j]);
+            Collections.sort(ap, p.slopeOrder());
+
+            for (int k = 0; k < ap.size() - 1; k++) {
+                ArrayList<Point> pl = new ArrayList<Point>();
+                pl.add(p);
+                Double s1 = p.slopeTo(ap.get(k));
+                pl.add(ap.get(k));
+                for (int j = k + 1; j < ap.size() && s1.equals(p.slopeTo(ap.get(j))); j++) {
+                    pl.add(ap.get(j));
+                }
+                if (pl.size() > 3) {
+                    Collections.sort(pl);
+                    LineSegment ls = new LineSegment(pl.get(0), pl.get(pl.size() - 1));
+                    for (Point point : pl) found.add(point);
+                    if (!existLS(ls)) {
+                        lsarr[couter] = ls;
+                        couter++;
+                    }
+
+
+                }
+
+            }
+
+
+        }
+    }
+
+    private static boolean exist(ArrayList<Point> arr, Point p) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i).equals(p)) return true;
+        }
+        return false;
+    }
+
+    private boolean existLS(LineSegment l) {
+        for (int i = 0; i < couter; i++) {
+            if (lsarr[i].toString().equals(l.toString())) return true;
+        }
+        return false;
+    }
+
+    public int numberOfSegments() {
+        return couter;
+    }
+
+    public LineSegment[] segments() {
+        LineSegment[] newls = new LineSegment[couter];
+        for (int i = 0; i < couter; i++) newls[i] = lsarr[i];
+        return newls;
+    }
+
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+
+
+    }
+
+
+}
